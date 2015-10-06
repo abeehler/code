a3de9e5f6949f3f71a82e2dd231e0d4edc0d4b5f
Andrew Beehler <abeehler@tamus.edu>
2015-10-06 15:40:55 -0500

    Pushing new changes

--- source/matching/ComparableMatchFactory.cs
+++ source/matching/ComparableMatchFactory.cs
@@ -8,10 +8,12 @@ namespace code.matching
     where AttributeType : IComparable<AttributeType>
   {
     IGetAnAttributeValue<Item, AttributeType> accessor;
+      MatchFactory<Item, AttributeType> matchFactory; 
 
     public ComparableMatchFactory(IGetAnAttributeValue<Item, AttributeType> accessor)
     {
       this.accessor = accessor;
+      this.matchFactory = new MatchFactory<Item, AttributeType>(accessor);
     }
 
     public IMatchAn<Item> greater_than(AttributeType value)
@@ -22,7 +24,22 @@ namespace code.matching
     public IMatchAn<Item> between(AttributeType start, AttributeType end)
     {
       return new AnonymousMatch<Item>(x => accessor(x).CompareTo(start) >= 0 &&
-      accessor(x).CompareTo(end) <=0);
+        accessor(x).CompareTo(end) <=0);
     }
-  }
+
+        public IMatchAn<Item> equal_to(AttributeType value)
+        {
+            return matchFactory.equal_to(value);
+        }
+
+        public IMatchAn<Item> equal_to_any(params AttributeType[] values)
+        {
+            return matchFactory.equal_to_any(values);
+        }
+
+        public IMatchAn<Item> not_equal_to(AttributeType value)
+        {
+            return matchFactory.not_equal_to(value);
+        }
+    }
 }
